44824d0ced1d2761a10b8580805b54785789657e
Nathanyel Sandi <nathan.sandi@gmail.com>
2026-06-19 14:05:11 -0300

    update libraries

--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("org.postgresql:postgresql:42.7.11")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation(project(":"))
 }
