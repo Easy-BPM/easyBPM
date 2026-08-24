0ba6ae42cf430dcf2353e04fc2b80e5fd7e1d30b
Nathanyel Sandi <nathan.sandi@gmail.com>
2026-08-24 17:03:24 -0300

    Melhorar welcome screen do modeler

--- easy-bpm-modeler/public/easybpm-config.js
+++ easy-bpm-modeler/public/easybpm-config.js
@@ -1,1 +1,4 @@
-window.__EASY_BPM_CONFIG__ = window.__EASY_BPM_CONFIG__ || {};
+window.__EASY_BPM_CONFIG__ = {
+  ...(window.__EASY_BPM_CONFIG__ || {}),
+  EASY_BPM_MODELER_AGENTIC_ORCHESTRATION: 'true'
+};
