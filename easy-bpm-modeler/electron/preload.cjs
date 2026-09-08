4735060e4a0ac4386e28ef19f2995198e05afbf8
Nathanyel Sandi <nathan.sandi@gmail.com>
2026-09-08 11:32:11 -0300

    Clarify desktop backend connection flow

--- easy-bpm-modeler/electron/preload.cjs
+++ easy-bpm-modeler/electron/preload.cjs
@@ -3,7 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('easyBpmDesktop', {
   config: {
     EASY_BPM_MODELER_RUNTIME_MODE: 'desktop',
-    EASY_BPM_MODELER_API_BASE_URL: process.env.EASY_BPM_MODELER_API_BASE_URL,
     EASY_BPM_MODELER_AGENTIC_ORCHESTRATION: process.env.EASY_BPM_MODELER_AGENTIC_ORCHESTRATION || 'true'
   },
   saveTextFile: (request) => ipcRenderer.invoke('easy-bpm:save-text-file', request),
